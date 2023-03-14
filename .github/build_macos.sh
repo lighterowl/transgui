23c269cc4d808d7d492a54b4d249d7773a568c32
Daniel Kamil Kozar <dkk089@gmail.com>
2023-03-15 00:04:01 +0100

    Use caching in actions (#5)

--- .github/build_macos.sh
+++ .github/build_macos.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 set -xe
-
 repo_dir=$PWD
 
 fixup_fpc_cfg() {
@@ -12,32 +11,40 @@ fixup_fpc_cfg() {
   echo '-k-F/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks' >> "$fpc_cfg_path"
 }
 
-brew install fpc
-cp /usr/local/etc/fpc.cfg ~/.fpc.cfg
-fixup_fpc_cfg ~/.fpc.cfg
+fpc_lazarus_build_install() {
+  brew install fpc
+  cp /usr/local/etc/fpc.cfg ~/.fpc.cfg
+  fixup_fpc_cfg ~/.fpc.cfg
 
-readonly fpc323_commit='0c5256300a323c78caa0b1a9cb772ac137f5aa8e'
-curl -O "https://gitlab.com/freepascal.org/fpc/source/-/archive/${fpc323_commit}/source-${fpc323_commit}.zip"
-unzip "source-${fpc323_commit}.zip"
-cd "source-${fpc323_commit}"
+  readonly fpc323_commit='0c5256300a323c78caa0b1a9cb772ac137f5aa8e'
+  curl -O "https://gitlab.com/freepascal.org/fpc/source/-/archive/${fpc323_commit}/source-${fpc323_commit}.tar.gz"
+  tar xf "source-${fpc323_commit}.tar.gz"
+  cd "source-${fpc323_commit}"
 
-make \
-  COMPILER_LIBRARYDIR='/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib' \
-  COMPILER_OPTIONS=-k-F/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks \
-  all
-mkdir -p ~/fpc-3.2.3
-make PREFIX=~/fpc-3.2.3 install
-export PATH=~/fpc-3.2.3/bin:~/fpc-3.2.3/lib/fpc/3.2.3:$PATH
+  make \
+    COMPILER_LIBRARYDIR='/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib' \
+    COMPILER_OPTIONS=-k-F/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks \
+    all
+  mkdir -p ~/fpc-3.2.3
+  make PREFIX=~/fpc-3.2.3 install
+  export PATH=~/fpc-3.2.3/bin:~/fpc-3.2.3/lib/fpc/3.2.3:$PATH
 
-fpcmkcfg -d basepath=${HOME}/fpc-3.2.3/lib/fpc/3.2.3 -o ~/.fpc.cfg
-fixup_fpc_cfg ~/.fpc.cfg
+  fpcmkcfg -d basepath=${HOME}/fpc-3.2.3/lib/fpc/3.2.3 -o ~/.fpc.cfg
+  fixup_fpc_cfg ~/.fpc.cfg
 
-cd
-curl -L -o lazarus-src.tar.gz 'https://sourceforge.net/projects/lazarus/files/Lazarus%20Zip%20_%20GZip/Lazarus%202.2.6/lazarus-2.2.6-0.tar.gz/download'
-tar xf lazarus-src.tar.gz
-cd lazarus
-make bigide
-export PATH=$PWD:$PATH
+  cd
+  curl -L -o lazarus-src.tar.gz 'https://sourceforge.net/projects/lazarus/files/Lazarus%20Zip%20_%20GZip/Lazarus%202.2.6/lazarus-2.2.6-0.tar.gz/download'
+  tar xf lazarus-src.tar.gz
+  cd lazarus
+  make bigide
+  export PATH=$PWD:$PATH
+}
+
+if [[ -f ~/.fpc.cfg && -d ~/lazarus && -d ~/fpc-3.2.3 ]]; then
+  export PATH=~/lazarus:~/fpc-3.2.3/bin:~/fpc-3.2.3/lib/fpc/3.2.3:$PATH
+else
+  fpc_lazarus_build_install
+fi
 
 cd "${repo_dir}/setup/macosx"
 source create_app_new.sh
--- .github/build_ubuntu.sh
+++ .github/build_ubuntu.sh
@@ -1,30 +1,38 @@
 #!/usr/bin/env bash
 
-set -e
+set -xe
 repo_dir=$PWD
 
-sudo apt update -yqq
-sudo apt install -yqq --no-install-recommends fpc build-essential
+fpc_lazarus_build_install() {
+  sudo apt update -yqq
+  sudo apt install -yqq --no-install-recommends fpc build-essential
 
-fpcmkcfg -d basepath=/usr/lib/x86_64-linux-gnu/fpc/3.2.2 -o ~/.fpc.cfg
+  fpcmkcfg -d basepath=/usr/lib/x86_64-linux-gnu/fpc/3.2.2 -o ~/.fpc.cfg
 
-readonly fpc323_commit='0c5256300a323c78caa0b1a9cb772ac137f5aa8e'
-curl -O "https://gitlab.com/freepascal.org/fpc/source/-/archive/${fpc323_commit}/source-${fpc323_commit}.tar.gz"
-tar xf "source-${fpc323_commit}.tar.gz"
-cd "source-${fpc323_commit}"
+  readonly fpc323_commit='0c5256300a323c78caa0b1a9cb772ac137f5aa8e'
+  curl -O "https://gitlab.com/freepascal.org/fpc/source/-/archive/${fpc323_commit}/source-${fpc323_commit}.tar.gz"
+  tar xf "source-${fpc323_commit}.tar.gz"
+  cd "source-${fpc323_commit}"
 
-make all
-mkdir -p ~/fpc-3.2.3
-make PREFIX=~/fpc-3.2.3 install
-export PATH=~/fpc-3.2.3/bin:~/fpc-3.2.3/lib/fpc/3.2.3:$PATH
-fpcmkcfg -d basepath=${HOME}/fpc-3.2.3/lib/fpc/3.2.3 -o ~/.fpc.cfg
+  make all
+  mkdir -p ~/fpc-3.2.3
+  make PREFIX=~/fpc-3.2.3 install
+  export PATH=~/fpc-3.2.3/bin:~/fpc-3.2.3/lib/fpc/3.2.3:$PATH
+  fpcmkcfg -d basepath=${HOME}/fpc-3.2.3/lib/fpc/3.2.3 -o ~/.fpc.cfg
 
-cd
-curl -L -o lazarus-src.tar.gz 'https://sourceforge.net/projects/lazarus/files/Lazarus%20Zip%20_%20GZip/Lazarus%202.2.6/lazarus-2.2.6-0.tar.gz/download'
-tar xf lazarus-src.tar.gz
-cd lazarus
-make bigide
-export PATH=$PWD:$PATH
+  cd
+  curl -L -o lazarus-src.tar.gz 'https://sourceforge.net/projects/lazarus/files/Lazarus%20Zip%20_%20GZip/Lazarus%202.2.6/lazarus-2.2.6-0.tar.gz/download'
+  tar xf lazarus-src.tar.gz
+  cd lazarus
+  make bigide
+  export PATH=$PWD:$PATH
+}
+
+if [[ -f ~/.fpc.cfg && -d ~/lazarus && -d ~/fpc-3.2.3 ]]; then
+  export PATH=~/lazarus:~/fpc-3.2.3/bin:~/fpc-3.2.3/lib/fpc/3.2.3:$PATH
+else
+  fpc_lazarus_build_install
+fi
 
 cd "$repo_dir"
-lazbuild transgui.lpi --lazarusdir=${HOME}/lazarus
+lazbuild transgui.lpi --build-mode=Release --lazarusdir=${HOME}/lazarus
