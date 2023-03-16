7adae9591906c95d7ac5068ff999a5979b276aff
Daniel Kamil Kozar <dkk089@gmail.com>
2023-03-16 18:13:01 +0100

    Produce AppImage in Ubuntu build

--- .github/build_ubuntu.sh
+++ .github/build_ubuntu.sh
@@ -1,40 +1,56 @@
 #!/usr/bin/env bash
 
 set -xe
-repo_dir=$PWD
+
+readonly repo_dir=$PWD
+readonly sdk_dir=~/.transgui_sdk
+readonly fpc_installdir="${sdk_dir}/fpc-3.2.3"
 
 fpc_lazarus_build_install() {
   sudo apt install -yqq --no-install-recommends fpc build-essential
 
   fpcmkcfg -d basepath=/usr/lib/x86_64-linux-gnu/fpc/3.2.2 -o ~/.fpc.cfg
 
+  cd "$sdk_dir"
   readonly fpc323_commit='0c5256300a323c78caa0b1a9cb772ac137f5aa8e'
   curl -O "https://gitlab.com/freepascal.org/fpc/source/-/archive/${fpc323_commit}/source-${fpc323_commit}.tar.gz"
   tar xf "source-${fpc323_commit}.tar.gz"
   cd "source-${fpc323_commit}"
 
   make all
-  mkdir -p ~/fpc-3.2.3
-  make PREFIX=~/fpc-3.2.3 install
-  export PATH=~/fpc-3.2.3/bin:~/fpc-3.2.3/lib/fpc/3.2.3:$PATH
-  fpcmkcfg -d basepath=${HOME}/fpc-3.2.3/lib/fpc/3.2.3 -o ~/.fpc.cfg
+  mkdir -p "${fpc_installdir}"
+  make PREFIX=$fpc_installdir install
+  export PATH=${fpc_installdir}/bin:${fpc_installdir}/lib/fpc/3.2.3:$PATH
+  fpcmkcfg -d basepath=${fpc_installdir}/lib/fpc/3.2.3 -p -o "${fpc_installdir}/etc/fpc.cfg"
+  mkdir -p "${fpc_installdir}/lib/fpc/etc"
+  ln -s ../../../etc/fpc.cfg "${fpc_installdir}/lib/fpc/etc/fpc.cfg"
 
-  cd
+  cd "$sdk_dir"
   curl -L -o lazarus-src.tar.gz 'https://sourceforge.net/projects/lazarus/files/Lazarus%20Zip%20_%20GZip/Lazarus%202.2.6/lazarus-2.2.6-0.tar.gz/download'
   tar xf lazarus-src.tar.gz
   cd lazarus
-  make bigide
+  make bigide LCL_PLATFORM=qt5
   export PATH=$PWD:$PATH
 }
 
 sudo apt update -yqq
-sudo apt install -yqq build-essential libgtk2.0-dev
+sudo apt install -yqq build-essential libqt5pas-dev
 
-if [[ -f ~/.fpc.cfg && -d ~/lazarus && -d ~/fpc-3.2.3 ]]; then
-  export PATH=~/lazarus:~/fpc-3.2.3/bin:~/fpc-3.2.3/lib/fpc/3.2.3:$PATH
+if [[ -d $sdk_dir ]]; then
+  export PATH=${sdk_dir}/lazarus:${fpc_installdir}/bin:${fpc_installdir}/lib/fpc/3.2.3:$PATH
 else
   fpc_lazarus_build_install
+  cd "$repo_dir"
 fi
 
-cd "$repo_dir"
-lazbuild transgui.lpi --build-mode=Release --lazarusdir=${HOME}/lazarus
+lazbuild transgui.lpi --ws=qt5 --build-mode=Release --lazarusdir=${sdk_dir}/lazarus
+cd units
+
+curl -L -O https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+chmod +x linuxdeploy-x86_64.AppImage
+curl -L -O https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
+chmod +x linuxdeploy-plugin-qt-x86_64.AppImage
+
+VERSION= ./linuxdeploy-x86_64.AppImage -e transgui --create-desktop-file \
+  --appdir AppDir --output appimage -i ../transgui.png --plugin qt
+sha256sum transgui--x86_64.AppImage
