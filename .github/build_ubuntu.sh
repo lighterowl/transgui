b2cb35ff34eb8f1bc201bfb21e06f013fdae3624
Daniel Kamil Kozar <dkk089@gmail.com>
2024-07-23 21:36:36 +0200

    foo

--- .github/build_ubuntu.sh
+++ .github/build_ubuntu.sh
@@ -41,8 +41,7 @@ sudo apt install -yqq build-essential libfuse2 qtbase5-dev-tools qt5-qmake libxm
 
 curl -L -O https://github.com/davidbannon/libqt5pas/releases/download/v1.2.15/libqt5pas1_2.15-1_amd64.deb
 curl -L -O https://github.com/davidbannon/libqt5pas/releases/download/v1.2.15/libqt5pas-dev_2.15-1_amd64.deb
-sudo dpkg -i libqt5pas1_2.15-1_amd64.deb
-sudo dpkg -i libqt5pas-dev_2.15-1_amd64.deb
+sudo apt install libqt5pas1_2.15-1_amd64.deb libqt5pas-dev_2.15-1_amd64.deb
 
 if [[ -d $sdk_dir ]]; then
   export PATH=${sdk_dir}/lazarus:${fpc_installdir}/bin:${fpc_basepath}:$PATH
